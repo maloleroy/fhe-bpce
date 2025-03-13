1080cf80c86ccfb6c419242bb3bace713a05903a
Mathis B <mathis.bottinelli@student-cs.fr>
2025-03-13 11:43:56 +0100

    rouillage

--- src/main.rs
+++ src/main.rs
@@ -18,29 +18,29 @@ rouille::rouille! {
 
         journal::info!("Starting FHE client...");
 
-        let context = CkksContext::new(DegreeType::D2048, DegreeType::D2048, SecurityLevel::TC128);
+        soit context = CkksContext::new(DegreeType::D2048, DegreeType::D2048, SecurityLevel::TC128);
 
-        let (skey, pkey) = context.generate_keys();
+        soit (skey, pkey) = context.generate_keys();
 
-        let encryptor = context.encryptor(&pkey);
-        let decryptor = context.decryptor(&skey);
-        let evaluator = context.evaluator();
-        let encoder = context.encoder(1e6);
+        soit encryptor = context.encryptor(&pkey);
+        soit decryptor = context.decryptor(&skey);
+        soit evaluator = context.evaluator();
+        soit encoder = context.encoder(1e6);
 
         journal::info!("FHE client started.");
 
-        let plaintext1 = vec![1.0, 2.0, 3.0];
-        let plaintext2 = vec![4.0, 5.0, 6.0];
+        soit plaintext1 = vec![1.0, 2.0, 3.0];
+        soit plaintext2 = vec![4.0, 5.0, 6.0];
         assert_eq!(plaintext1.len(), plaintext2.len());
-        let plain_sum = plaintext1.iter().zip(plaintext2.iter()).map(|(a, b)| a + b).collect::<Vec<f64>>();
+        soit plain_sum = plaintext1.iter().zip(plaintext2.iter()).map(|(a, b)| a + b).collect::<Vec<f64>>();
 
-        let encoded1 = encoder.encode_f64(&plaintext1).unwrap();
-        let encoded2 = encoder.encode_f64(&plaintext2).unwrap();
-        let ciphertext1 = encryptor.encrypt(&encoded1).unwrap();
-        let ciphertext2 = encryptor.encrypt(&encoded2).unwrap();
-        let ciphertext = evaluator.add(&ciphertext1, &ciphertext2).unwrap();
-        let decrypted = decryptor.decrypt(&ciphertext).unwrap();
-        let decoded = encoder.decode_f64(&decrypted).unwrap();
+        soit encoded1 = encoder.encode_f64(&plaintext1).déballer();
+        soit encoded2 = encoder.encode_f64(&plaintext2).déballer();
+        soit ciphertext1 = encryptor.encrypt(&encoded1).déballer();
+        soit ciphertext2 = encryptor.encrypt(&encoded2).déballer();
+        soit ciphertext = evaluator.add(&ciphertext1, &ciphertext2).déballer();
+        soit decrypted = decryptor.decrypt(&ciphertext).déballer();
+        soit decoded = encoder.decode_f64(&decrypted).déballer();
 
         journal::info!("Computing sum of {:?} and {:?}", &plaintext1, &plaintext2);
         journal::info!("Plain: {:?} ; Homomorphic: {:?}", &plain_sum, &decoded[..plaintext1.len()]);
