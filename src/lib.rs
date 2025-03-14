7e4f2e0891ea4b5f0f3ac4a05bfb173a83b03513
Mathis BOTTINELLI <mathisbato@Mathiss-MacBook-Air.local>
2025-03-14 10:16:04 +0100

    final api skeleton

--- src/lib.rs
+++ src/lib.rs
@@ -2,21 +2,45 @@
 #![warn(clippy::nursery, clippy::pedantic)]
 #![forbid(unsafe_code)]
 
-pub mod config;
 pub mod ops;
 
 use bincode::{Decode, Encode};
 
 #[derive(Encode)]
+#[non_exhaustive]
 /// A wrapper around the different homomorphic encryption ciphertexts.
 pub enum Ciphertext {
     Seal(seal_lib::Ciphertext),
 }
 
 #[derive(Clone)]
+#[non_exhaustive]
 /// A wrapper around the different homomorphic encryption contexts.
 pub enum Context {
     Seal(seal_lib::context::CkksContext),
+}
+
+#[non_exhaustive]
+pub enum Encryptor {
+    SealCkks {
+        encoder: seal_lib::CKKSEncoder,
+        encryptor: seal_lib::Encryptor,
+    },
+    SealBfv {
+        encoder: seal_lib::BFVEncoder,
+        encryptor: seal_lib::Encryptor,
+    },
+}
+
+#[non_exhaustive]
+pub enum Decryptor {
+    Seal(seal_lib::Decryptor),
+}
+
+#[non_exhaustive]
+pub enum Evaluator {
+    SealCkks(seal_lib::CKKSEvaluator),
+    SealBfv(seal_lib::BFVEvaluator),
 }
 
 impl Decode<Context> for Ciphertext {
@@ -36,7 +60,6 @@ impl Decode<Context> for Ciphertext {
 #[derive(Encode)]
 /// The data that will be exchanged by the client and the server.
 struct ExchangeData {
-    // FIXME: Replace with ciphertext struct
     lhs: Vec<Ciphertext>,
     rhs: Vec<Option<Ciphertext>>,
     operation: Vec<ops::Operation>,
