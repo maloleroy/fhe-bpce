4d091f403617ca5ab43d8489f3e12b54d1cbcdee
fluteur <malo.leroy@student-cs.fr>
2025-03-17 15:08:11 +0100

    Fix formatting in fhe-select

--- fhe-select/src/lib.rs
+++ fhe-select/src/lib.rs
@@ -4,4 +4,4 @@
 
 mod selectable_collection;
 
-pub use selectable_collection::{SelectableCollection, SelectableItem, FLAG_ON, FLAG_OFF};
+pub use selectable_collection::{FLAG_OFF, FLAG_ON, SelectableCollection, SelectableItem};
