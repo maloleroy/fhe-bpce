87957b42ddde48be460d7eaa78f3b5997e71ce2e
Mathis B <mathis.bottinelli@student-cs.fr>
2025-03-19 11:31:57 +0100

    sending/receiving data

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,22 @@
+#![forbid(unsafe_code)]
+#![warn(clippy::nursery, clippy::pedantic)]
+#![allow(clippy::missing_panics_doc)]
+
 use core::net::SocketAddr;
+use tokio::io::AsyncWriteExt;
 use tokio::net::{TcpListener, TcpStream};
 
 mod server;
 
 pub async fn start_client(socket_addr: SocketAddr) {
-    let _stream = TcpStream::connect(socket_addr)
+    let mut stream = TcpStream::connect(socket_addr)
         .await
         .expect("Failed to connect to server");
+
+    stream
+        .write_all(b"Hello, world!")
+        .await
+        .expect("Failed to send data to server");
 }
 
 pub async fn start_server(port: u16) {
--- src/server.rs
+++ src/server.rs
@@ -1,3 +1,15 @@
-use tokio::net::TcpStream;
+use tokio::{io::AsyncReadExt, net::TcpStream};
 
-pub async fn handle_client(_stream: TcpStream) {}
+pub async fn handle_client(mut stream: TcpStream) {
+    let mut buf = [0; 1024];
+
+    let bytes_read = stream
+        .read(&mut buf)
+        .await
+        .expect("Failed to read data from client");
+
+    log::info!(
+        "Received data from client: {:?}",
+        String::from_utf8_lossy(&buf[..bytes_read])
+    );
+}
