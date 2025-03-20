1c99e1502c4ee074fdb8a9c40169b723c83bc868
Mathis BOTTINELLI <mathisbato@Mathiss-MacBook-Air.local>
2025-03-20 11:35:05 +0100

    cli: better error handling

--- src/lib.rs
+++ src/lib.rs
@@ -8,28 +8,44 @@ use tokio::net::{TcpListener, TcpStream};
 
 mod server;
 
-pub async fn start_client(socket_addr: SocketAddr) {
-    let mut stream = TcpStream::connect(socket_addr)
-        .await
-        .expect("Failed to connect to server");
+/// On error, log the error and abort the process.
+macro_rules! ensure {
+    ($expr:expr) => {
+        match $expr {
+            Ok(val) => val,
+            Err(err) => {
+                ::log::error!("FATAL: {}", err);
+                ::std::process::exit(1);
+            }
+        }
+    };
+}
 
-    stream
-        .write_all(b"Hello, world!")
-        .await
-        .expect("Failed to send data to server");
+/// On error, log the error and execute the fallback instruction.
+macro_rules! faillible {
+    ($expr:expr, $fallback:expr) => {
+        match $expr {
+            Ok(val) => val,
+            Err(err) => {
+                ::log::error!("{}", err);
+                $fallback
+            }
+        }
+    };
+}
+
+pub async fn start_client(socket_addr: SocketAddr) {
+    let mut stream = ensure!(TcpStream::connect(socket_addr).await);
+
+    ensure!(stream.write_all(b"Hello, world!").await);
 }
 
 pub async fn start_server(port: u16) {
     let socket_addr = SocketAddr::new([0, 0, 0, 0].into(), port);
-    let listener = TcpListener::bind(socket_addr)
-        .await
-        .expect("Failed to bind to port");
+    let listener = ensure!(TcpListener::bind(socket_addr).await);
 
     loop {
-        let (stream, client_addr) = listener
-            .accept()
-            .await
-            .expect("Failed to accept connection");
+        let (stream, client_addr) = faillible!(listener.accept().await, continue);
 
         tokio::spawn(async move {
             log::info!("Accepted connection from {}", client_addr);
