982104afbcb60596f5e29615de10290a4f18d85c
fluteur <malo.leroy@student-cs.fr>
2025-03-21 11:06:38 +0100

    Hotfix for SelectableCollection
    Sorry, I used the nightly version of Clippy

--- fhe-select/src/selectable_collection.rs
+++ fhe-select/src/selectable_collection.rs
@@ -55,13 +55,13 @@ impl<const F: usize, C: SelectableCS<Ciphertext: Clone>> SelectableCollection<F,
 
     #[must_use]
     #[inline]
-    pub const fn len(&self) -> usize {
+    pub fn len(&self) -> usize {
         self.items.len()
     }
 
     #[must_use]
     #[inline]
-    pub const fn is_empty(&self) -> bool {
+    pub fn is_empty(&self) -> bool {
         self.items.is_empty()
     }
 
