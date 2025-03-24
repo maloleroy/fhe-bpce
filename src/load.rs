07dd27bce680dfbead6ae204d4b5c8398cb2f9c6
Mathis B <mathis.bottinelli@student-cs.fr>
2025-03-24 14:48:57 +0100

    More customizable data sent

--- src/load.rs
+++ src/load.rs
@@ -1,18 +1,22 @@
 //! Data loading utilities.
 #![allow(dead_code)]
 
-pub mod bytes;
 pub mod csv;
 pub mod json;
 #[cfg(feature = "parquet")]
 pub mod parquet;
 
+use bincode::Encode;
+use fhe_core::api::CryptoSystem;
+use fhe_exchange::ExchangeData;
 use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum DataError {
     #[error("IO error: {0}")]
-    Io(#[from] tokio::io::Error),
+    Io(#[from] std::io::Error),
+    #[error("Parsing error")]
+    Parsing,
     #[error("Unsupported format")]
     UnsupportedFormat,
     #[error("Unknown error")]
@@ -21,6 +25,10 @@ pub enum DataError {
 
 pub type DataResult<T> = Result<T, DataError>;
 
-pub trait DataLoader<T> {
-    async fn load(file: tokio::fs::File) -> DataResult<T>;
+pub trait DataLoader<C: CryptoSystem>
+where
+    C::Operation: Encode,
+    C::Ciphertext: Encode,
+{
+    fn load(file: std::fs::File, cs: &C) -> DataResult<ExchangeData<C>>;
 }
