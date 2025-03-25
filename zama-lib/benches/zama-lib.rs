0d4d7e12fbe2d5d45784b1dc8397e35757fef362
Mathis B <mathis.bottinelli@student-cs.fr>
2025-03-25 15:39:34 +0100

    feat(tfhe): benches + small fixes

---
+++ zama-lib/benches/zama-lib.rs
@@ -0,0 +1,45 @@
+use criterion::{Criterion, criterion_group, criterion_main};
+use fhe_core::api::CryptoSystem;
+use zama_lib::{FheUint32, TfheHOperation, ZamaTfheUintCS, config::ZamaTfheContext};
+
+fn benchmark_tfhe(c: &mut Criterion) {
+    let ctx = ZamaTfheContext::new();
+    let cipher = ZamaTfheUintCS::<u32, _, FheUint32>::new(ctx);
+
+    let input = 42;
+
+    c.bench_function("tfhe cipher", |b| {
+        b.iter(|| {
+            cipher.cipher(&input);
+        })
+    });
+
+    let input2 = 42;
+    let ciphered_input = cipher.cipher(&input);
+    let ciphered_input2 = cipher.cipher(&input2);
+
+    c.bench_function("tfhe add", |b| {
+        b.iter(|| {
+            let _ = cipher.operate(TfheHOperation::Add, &ciphered_input, Some(&ciphered_input2));
+        })
+    });
+
+    c.bench_function("tfhe mul", |b| {
+        b.iter(|| {
+            let _ = cipher.operate(TfheHOperation::Mul, &ciphered_input, Some(&ciphered_input2));
+        })
+    });
+
+    c.bench_function("tfhe decipher", |b| {
+        b.iter(|| {
+            cipher.decipher(&ciphered_input);
+        })
+    });
+}
+
+criterion_group!(
+    name = zama_lib_benchmarks;
+    config = Criterion::default().measurement_time(core::time::Duration::from_secs(5));
+    targets = benchmark_tfhe
+);
+criterion_main!(zama_lib_benchmarks);
