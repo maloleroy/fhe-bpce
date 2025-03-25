b1710d134ff039fc9ac418f011b32bf874e12e51
Mathis BOTTINELLI <mathisbato@Mathiss-MacBook-Air.local>
2025-03-25 15:21:49 +0100

    feat(tfhe): support for TFHE scheme 🍋

---
+++ zama-lib/src/config.rs
@@ -0,0 +1,118 @@
+use bincode::{Decode, Encode, serde::Compat};
+use tfhe::{ClientKey, Config, ConfigBuilder, generate_keys};
+
+#[derive(Clone)]
+/// A server key for TFHE operations.   
+pub struct ServerKey(pub(super) tfhe::ServerKey);
+
+impl Encode for ServerKey {
+    fn encode<E: bincode::enc::Encoder>(
+        &self,
+        encoder: &mut E,
+    ) -> Result<(), bincode::error::EncodeError> {
+        let (skey, ksm, ck, dk, tag) = self.0.clone().into_raw_parts();
+
+        let compat_skey = Compat(skey);
+        let compat_ksm = Compat(ksm);
+        let compat_ck = Compat(ck);
+        let compat_dk = Compat(dk);
+        let compat_tag = Compat(tag);
+        compat_skey.encode(encoder)?;
+        compat_ksm.encode(encoder)?;
+        compat_ck.encode(encoder)?;
+        compat_dk.encode(encoder)?;
+        compat_tag.encode(encoder)?;
+
+        Ok(())
+    }
+}
+
+impl<Context> Decode<Context> for ServerKey {
+    fn decode<D: bincode::de::Decoder<Context = Context>>(
+        decoder: &mut D,
+    ) -> Result<Self, bincode::error::DecodeError> {
+        let compat_skey = Compat::decode(decoder)?;
+        let compat_ksm = Compat::decode(decoder)?;
+        let compat_ck = Compat::decode(decoder)?;
+        let compat_dk = Compat::decode(decoder)?;
+        let compat_tag = Compat::decode(decoder)?;
+
+        let skey = compat_skey.0;
+        let ksm = compat_ksm.0;
+        let ck = compat_ck.0;
+        let dk = compat_dk.0;
+        let tag = compat_tag.0;
+
+        Ok(ServerKey(tfhe::ServerKey::from_raw_parts(
+            skey, ksm, ck, dk, tag,
+        )))
+    }
+}
+
+#[derive(Clone)]
+/// A context for TFHE operations.
+pub struct ZamaTfheContext {
+    config: Config,
+    server_key: Option<ServerKey>,
+}
+
+impl ZamaTfheContext {
+    #[must_use]
+    /// Create a new TFHE context.
+    pub fn new() -> Self {
+        Self {
+            config: ConfigBuilder::default().build(),
+            server_key: None,
+        }
+    }
+
+    #[must_use]
+    /// Create a TFHE scheme knowing the server key.
+    pub fn from_server_key(server_key: ServerKey) -> Self {
+        Self {
+            config: ConfigBuilder::default().build(),
+            server_key: Some(server_key),
+        }
+    }
+
+    #[must_use]
+    #[inline]
+    /// Get the server key.
+    ///
+    /// # Panics
+    ///
+    /// Panics if the server key is not set (the configuration is new).
+    pub const fn server_key(&self) -> Option<&ServerKey> {
+        self.server_key.as_ref()
+    }
+
+    #[must_use]
+    #[inline]
+    /// Generate a set of secret, public and relinearization keys.
+    pub fn generate_keys(&self) -> (Option<ClientKey>, ServerKey) {
+        match &self.server_key {
+            Some(server_key) => (None, server_key.clone()),
+            None => {
+                let (client_key, server_key) = generate_keys(self.config);
+                (Some(client_key), ServerKey(server_key))
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::ZamaTfheContext;
+
+    const CONFIG: bincode::config::Configuration = bincode::config::standard();
+
+    #[test]
+    fn test_skey_serde() {
+        let ctx = ZamaTfheContext::new();
+        let (_ck, skey) = ctx.generate_keys();
+
+        let encoded = bincode::encode_to_vec(&skey, CONFIG).unwrap();
+        let (_decoded, _): (super::ServerKey, _) =
+            bincode::decode_from_slice(&encoded, CONFIG).unwrap();
+    }
+}
---
+++ zama-lib/src/lib.rs
@@ -0,0 +1,164 @@
+//! Convenient wrapper around Microsoft SEAL library.
+#![no_std]
+
+extern crate alloc;
+
+pub use bincode::{Decode, Encode};
+use core::ops::{Add, Mul};
+use fhe_core::api::{CryptoSystem, select::SelectableCS};
+use tfhe::{
+    ClientKey,
+    prelude::{FheDecrypt, FheEncrypt},
+    set_server_key,
+};
+
+pub use tfhe::{FheUint, FheUint8, FheUint16, FheUint32, FheUint64, FheUint128};
+
+pub mod config;
+
+#[derive(Clone)]
+/// Ciphertext from Microsoft SEAL.
+pub struct Ciphertext<T, Key, I: FheEncrypt<T, Key>> {
+    value: I,
+    _phantom: core::marker::PhantomData<(T, Key)>,
+}
+
+impl<T, Key, I: FheEncrypt<T, Key> + Encode> Encode for Ciphertext<T, Key, I> {
+    fn encode<E: bincode::enc::Encoder>(
+        &self,
+        encoder: &mut E,
+    ) -> Result<(), bincode::error::EncodeError> {
+        self.value.encode(encoder)
+    }
+}
+
+impl<Context, T, Key, I: FheEncrypt<T, Key> + Encode + Decode<Context>> Decode<Context>
+    for Ciphertext<T, Key, I>
+{
+    fn decode<D: bincode::de::Decoder<Context = Context>>(
+        decoder: &mut D,
+    ) -> Result<Self, bincode::error::DecodeError> {
+        Ok(Self {
+            value: I::decode(decoder)?,
+            _phantom: core::marker::PhantomData,
+        })
+    }
+}
+
+/// The TFHE CryptoSystem backed by Zama, for unsigned integers.
+pub struct ZamaTfheUintCS<T, Key, I: FheEncrypt<T, Key>> {
+    client_key: Option<tfhe::ClientKey>,
+    _phantom: core::marker::PhantomData<(T, Key, I)>,
+}
+
+impl<T, Key, I: FheEncrypt<T, Key>> ZamaTfheUintCS<T, Key, I> {
+    pub fn new(context: config::ZamaTfheContext) -> Self {
+        let (client_key, secret_key) = context.generate_keys();
+        set_server_key(secret_key.0.clone());
+        Self {
+            client_key,
+            _phantom: core::marker::PhantomData,
+        }
+    }
+}
+
+impl<T: Copy, I: FheEncrypt<T, ClientKey> + FheDecrypt<T> + Add + Mul + Clone> CryptoSystem
+    for ZamaTfheUintCS<T, ClientKey, I>
+where
+    I: Add<Output = I>,
+    I: Mul<Output = I>,
+{
+    type Ciphertext = Ciphertext<T, ClientKey, I>;
+    type Plaintext = T;
+    type Operation = TfheHOperation;
+
+    fn cipher(&self, plaintext: &Self::Plaintext) -> Self::Ciphertext {
+        let ciphertext = I::encrypt(*plaintext, self.client_key.as_ref().unwrap());
+        Ciphertext {
+            value: ciphertext,
+            _phantom: core::marker::PhantomData,
+        }
+    }
+
+    fn decipher(&self, ciphertext: &Self::Ciphertext) -> Self::Plaintext {
+        ciphertext.value.decrypt(self.client_key.as_ref().unwrap())
+    }
+
+    fn operate(
+        &self,
+        operation: Self::Operation,
+        lhs: &Self::Ciphertext,
+        rhs: Option<&Self::Ciphertext>,
+    ) -> Self::Ciphertext {
+        match operation {
+            TfheHOperation::Add => {
+                let rhs = rhs.expect("Addition requires two operands.");
+                let result = lhs.value.clone() + rhs.value.clone();
+                Ciphertext {
+                    value: result,
+                    _phantom: core::marker::PhantomData,
+                }
+            }
+            TfheHOperation::Mul => {
+                let rhs = rhs.expect("Multiplication requires two operands.");
+                let result = lhs.value.clone() * rhs.value.clone();
+                Ciphertext {
+                    value: result,
+                    _phantom: core::marker::PhantomData,
+                }
+            }
+        }
+    }
+
+    #[inline]
+    fn relinearize(&self, _ciphertext: &mut Self::Ciphertext) {
+        // No-op
+    }
+}
+
+impl<I: FheEncrypt<u64, ClientKey> + FheDecrypt<u64> + Add + Mul + Clone> SelectableCS
+    for ZamaTfheUintCS<u64, ClientKey, I>
+where
+    I: Add<Output = I>,
+    I: Mul<Output = I>,
+{
+    fn flag_to_plaintext(&self, flag: fhe_core::api::select::Flag) -> Self::Plaintext {
+        const FLAG_ON: u64 = 1;
+        const FLAG_OFF: u64 = 0;
+
+        match flag {
+            fhe_core::api::select::Flag::On => FLAG_ON,
+            fhe_core::api::select::Flag::Off => FLAG_OFF,
+        }
+    }
+}
+
+#[derive(Clone, Copy, Debug, Encode, Decode)]
+#[non_exhaustive]
+pub enum TfheHOperation {
+    Add,
+    Mul,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::config::ZamaTfheContext;
+
+    #[test]
+    fn test_tfhe() {
+        let context = ZamaTfheContext::new();
+        let cs = ZamaTfheUintCS::<u8, _, FheUint8>::new(context);
+
+        let a = cs.cipher(&27);
+        let b = cs.cipher(&128);
+
+        let result = cs.operate(TfheHOperation::Add, &a, Some(&b));
+
+        let decrypted_result = cs.decipher(&result);
+
+        let clear_result = 27 + 128;
+
+        assert_eq!(decrypted_result, clear_result);
+    }
+}
