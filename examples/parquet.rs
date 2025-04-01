7e272e28e29ac10da2e5e22f00c7b20c009f6477
Mathis B <mathis.bottinelli@student-cs.fr>
2025-04-01 17:22:13 +0200

    fix fmt

--- examples/parquet.rs
+++ examples/parquet.rs
@@ -2,14 +2,18 @@ use arrow::array::Float64Array;
 use fhe_core::api::CryptoSystem as _;
 use parquet::arrow::arrow_reader::ParquetRecordBatchReaderBuilder;
 use rayon::iter::{ParallelBridge, ParallelIterator};
-use seal_lib::{CkksHOperation2, SealCkksCS, context::SealCkksContext};
+use seal_lib::{BfvHOperation2, SealBfvCS, context::SealBFVContext};
 use std::sync::Mutex;
 
 const BATCH_SIZE: usize = 1 << 15; // 32 KB
 
 fn main() {
-    let bfv_ctx = SealCkksContext::new(seal_lib::DegreeType::D4096, seal_lib::SecurityLevel::TC128);
-    let bfv_cs = SealCkksCS::new(&bfv_ctx, 307727.0);
+    let bfv_ctx = SealBFVContext::new(
+        seal_lib::DegreeType::D4096,
+        seal_lib::SecurityLevel::TC128,
+        16,
+    );
+    let bfv_cs = SealBfvCS::new(&bfv_ctx);
 
     let file = std::fs::File::open("data.parquet").unwrap();
     let parquet_reader = ParquetRecordBatchReaderBuilder::try_new(file)
@@ -20,7 +24,7 @@ fn main() {
 
     let iter = parquet_reader.into_iter();
 
-    let sum = Mutex::new(bfv_cs.cipher(&0.0));
+    let sum = Mutex::new(bfv_cs.cipher(&0));
 
     iter.par_bridge().for_each(|batch| {
         let batch = batch.unwrap();
@@ -38,20 +42,21 @@ fn main() {
             .downcast_ref::<Float64Array>()
             .expect("Column is not Float64Array");
 
-        let mut local_sum = bfv_cs.cipher(&0.0);
+        let mut local_sum = bfv_cs.cipher(&0);
 
         for i in 0..array.len() {
-            let v_cipher = bfv_cs.cipher(&array.value(i));
-            bfv_cs.operate2_inplace(CkksHOperation2::Add, &mut local_sum, &v_cipher);
+            let value = array.value(i);
+            let value_uint = (value * 100.0) as u64;
+            let v_cipher = bfv_cs.cipher(&value_uint);
+            bfv_cs.operate2_inplace(BfvHOperation2::Add, &mut local_sum, &v_cipher);
         }
 
         println!("Summed {} f64 of the column \"rwa\".", BATCH_SIZE);
 
         let mut sum_lock = sum.lock().unwrap();
-        bfv_cs.operate2_inplace(CkksHOperation2::Add, &mut sum_lock, &local_sum);
+        bfv_cs.operate2_inplace(BfvHOperation2::Add, &mut sum_lock, &local_sum);
     });
 
     let sum_d = bfv_cs.decipher(&sum.lock().unwrap());
     println!("Sum of all f64 of the column \"rwa\": {}", sum_d);
 }
-
